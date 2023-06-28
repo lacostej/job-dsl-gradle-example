b60cbe0c63c5483e628e98085748074a655088f0
Michal <m.nakiewicz@gmail.com>
2023-06-28 19:51:13 +0200

    GitHub initial test re-introduced and seems to move forward however still failing.

--- src/test/groovy/com/dslexample/JobScriptsSpec.groovy
+++ src/test/groovy/com/dslexample/JobScriptsSpec.groovy
@@ -1,5 +1,10 @@
 package com.dslexample
 
+import com.dslexample.support.TestUtil
+import javaposse.jobdsl.dsl.DslScriptLoader
+import javaposse.jobdsl.dsl.GeneratedItems
+import javaposse.jobdsl.dsl.GeneratedJob
+import javaposse.jobdsl.dsl.JobManagement
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -7,10 +12,28 @@ class JobScriptsSpec extends Specification {
 
     @Unroll
     void 'test script #file.name'(File file) {
-        expect:
-        System.out.println(file.name)
+        given:
+        JobManagement jm = Mock(JobManagement)
+
+        when:
+        GeneratedItems items = new DslScriptLoader(jm).runScript(file.text)
+        writeItems(items, new File('output'))
+
+        then:
+        noExceptionThrown()
 
         where:
-        file << new File('./src/jobs').listFiles().findAll { it.name.endsWith('.groovy') }
+        file << TestUtil.getJobFiles()
+    }
+
+    /**
+     * Write the config.xml for each generated job and view to the build dir.
+     */
+    private void writeItems(GeneratedItems items, File outputDir) {
+        items.jobs.each { GeneratedJob generatedJob ->
+            String jobName = generatedJob.jobName
+            String xml = jm.getConfig(jobName)
+            TestUtil.writeFile(new File(outputDir, 'jobs'), jobName, xml)
+        }
     }
 }
