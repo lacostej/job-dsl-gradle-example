7c2fb163d2d0543fa168636c8e0ec1ccf0130b7a
Michal <m.nakiewicz@gmail.com>
2023-06-28 19:59:30 +0200

    Amend to previous commit: GitHub initial test re-introduced and seems to move forward however still failing.

--- src/test/groovy/com/dslexample/JobScriptsSpec.groovy
+++ src/test/groovy/com/dslexample/JobScriptsSpec.groovy
@@ -1,23 +1,42 @@
 package com.dslexample
 
 import com.dslexample.support.TestUtil
+import hudson.model.Item
+import hudson.model.View
 import javaposse.jobdsl.dsl.DslScriptLoader
 import javaposse.jobdsl.dsl.GeneratedItems
 import javaposse.jobdsl.dsl.GeneratedJob
+import javaposse.jobdsl.dsl.GeneratedView
 import javaposse.jobdsl.dsl.JobManagement
+import javaposse.jobdsl.plugin.JenkinsJobManagement
+import jenkins.model.Jenkins
+import org.junit.ClassRule
+import org.jvnet.hudson.test.JenkinsRule
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
 class JobScriptsSpec extends Specification {
 
+    @Shared
+    @ClassRule
+    private JenkinsRule jenkinsRule = new JenkinsRule()
+
+    @Shared
+    private File outputDir = new File('./build/debug-xml')
+
+    def setupSpec() {
+        outputDir.deleteDir()
+    }
+
     @Unroll
     void 'test script #file.name'(File file) {
         given:
-        JobManagement jm = Mock(JobManagement)
+        JobManagement jm = new JenkinsJobManagement(System.out, [:], new File('.'))
 
         when:
         GeneratedItems items = new DslScriptLoader(jm).runScript(file.text)
-        writeItems(items, new File('output'))
+        writeItems(items, outputDir)
 
         then:
         noExceptionThrown()
@@ -30,10 +49,12 @@ class JobScriptsSpec extends Specification {
      * Write the config.xml for each generated job and view to the build dir.
      */
     private void writeItems(GeneratedItems items, File outputDir) {
+        Jenkins jenkins = jenkinsRule.jenkins
         items.jobs.each { GeneratedJob generatedJob ->
             String jobName = generatedJob.jobName
-            String xml = jm.getConfig(jobName)
-            TestUtil.writeFile(new File(outputDir, 'jobs'), jobName, xml)
+            Item item = jenkins.getItemByFullName(jobName)
+            String text = new URL(jenkins.rootUrl + item.url + 'config.xml').text
+            TestUtil.writeFile(new File(outputDir, 'jobs'), jobName, text)
         }
     }
 }
